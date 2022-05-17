b417bdcdadcb1cb84e76fe37c59d3b6164fdd33e
ali m alghanay <52647886+alialghanay@users.noreply.github.com>
2022-05-17 10:21:45 +0000

    dotenv

--- myApp.js
+++ myApp.js
@@ -1,9 +1,16 @@
 let express = require('express');
 let app = express();
 const absolutePath = __dirname + "/views/index.html";
-app.use("/public", express.static(__dirname + "/public"));
+
 app.get('/', (req, res) => {
   res.sendfile(absolutePath);
+});
+app.use('/public', express.static( __dirname + '/public' ));
+
+app.get('/json', (req, res) => {
+  if(process.env['MESSAGE_STYLE'] === 'uppercase'){
+    res.json({"message": "HELLO JSON"})
+  }else {res.json({"message": "Hello json"})}
 });
 
 
