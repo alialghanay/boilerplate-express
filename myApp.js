63526d0937dd8eb99f8610040b85c97ef2640926
ali m alghanay <52647886+alialghanay@users.noreply.github.com>
2022-05-17 07:37:27 +0000

    serve-static-assets

--- myApp.js
+++ myApp.js
@@ -1,13 +1,10 @@
 let express = require('express');
 let app = express();
 const absolutePath = __dirname + "/views/index.html";
+app.use("/public", express.static(__dirname + "/public"));
 app.get('/', (req, res) => {
   res.sendfile(absolutePath);
-})
-
-
-
-
+});
 
 
 
