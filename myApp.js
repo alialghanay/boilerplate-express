6b9e02211be3fd4a1673bd0613326312e6655415
ali m alghanay <52647886+alialghanay@users.noreply.github.com>
2022-05-22 14:31:19 +0000

    root-level

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 let express = require('express');
 let app = express();
 const absolutePath = __dirname + "/views/index.html";
-
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+})
 app.get('/', (req, res) => {
   res.sendfile(absolutePath);
 });
